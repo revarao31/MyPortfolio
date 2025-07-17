482bea83c41316a338744325ff4d9ca443dcd10e
revarao31 <revarao31@gmail.com>
2025-07-18 00:15:41 +0530

    Added Files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ THIS LINE IS IMPORTANT FOR VERCEL
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
